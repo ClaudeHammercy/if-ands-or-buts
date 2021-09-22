1aa2d0e869db2ce9ea8bdfd52de689188fce6c37
Rashad Gant <83968751+rashadgant@users.noreply.github.com>
2021-09-22 15:18:49 -0400

    TrippleContd

--- tripple-bill-splitter.js
+++ tripple-bill-splitter.js
@@ -3,3 +3,17 @@ const friendOne = Number(prompt('How much did Friend One pay?  '));
 const friendTwo = Number(prompt('How much did Friend Two pay?  '));
 const friendThree = Number(prompt('How much did Friend Three pay?  '));
 const total = Number(friendTwo + friendOne + friendThree);
+const split = total / 3;
+
+if (split > friendOne) {
+    const shareOne = split - friendOne;
+    console.log('Friend One owes Friends Two and Three ', shareOne, ' apiece.');
+}
+if (split > friendTwo) {
+    const shareTwo = split - friendTwo;
+    console.log('Friend Two owes Friends One and Three ', shareOTwo, ' apiece.');
+}
+if (split > friendThree) {
+    const shareThree = split - friendThree;
+    console.log('Friend Three owes Friends One and Two ', shareOTwo, ' apiece.');
+}
